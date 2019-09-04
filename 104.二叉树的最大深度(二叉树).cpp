b8e1335757f5fbf05b586a674ea6a4e11aeab484
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2019-09-04 10:34:13 +0800

    Create 104.二叉树的最大深度(二叉树).cpp

---
+++ 104.二叉树的最大深度(二叉树).cpp
@@ -0,0 +1,34 @@
+//递归
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
+
+//BFS
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int num = 0;
+        queue<TreeNode *> que;
+        que.push(root);
+        while(!que.empty()){
+            int n = que.size();
+            for(int i = 0;i < n;++i){
+                TreeNode *cur = que.front();
+                if(cur->left != NULL)
+                    que.push(cur->left);
+                if(cur->right != NULL)
+                    que.push(cur->right);
+                que.pop();
+            }
+            num++;
+        }
+        return num;
+    }
+};
