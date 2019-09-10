4e07090c79d6d6d0c8549851eeb437a9cd24a6f9
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2019-09-10 15:18:23 +0800

    Update 59.螺旋矩阵(数组).cpp

--- 59.螺旋矩阵(数组).cpp
+++ 59.螺旋矩阵(数组).cpp
@@ -24,7 +24,7 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> ans(n, vector<int>(n, 0));
         int c = 1, j = 0;
-        while(c < n * n) {
+        while(c <= n * n) {
             for(int i = j; i < n - j; i++ ) {
                 ans[j][i] = c++;
             }
