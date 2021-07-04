e0563589380fecc71083dd9c1ab00e24b76d572b
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-04 16:56:24 +0800

    Update 剑指09.java

--- 剑指09.java
+++ 剑指09.java
@@ -1,3 +1,21 @@
+/*成员变量
+
+维护两个栈 stack1 和 stack2，其中 stack1 支持插入操作，stack2 支持删除操作
+构造方法
+
+初始化 stack1 和 stack2 为空
+插入元素
+
+插入元素对应方法 appendTail
+
+stack1 直接插入元素
+删除元素
+
+删除元素对应方法 deleteHead
+
+如果 stack2 为空，则将 stack1 里的所有元素弹出插入到 stack2 里
+如果 stack2 仍为空，则返回 -1，否则从 stack2 弹出一个元素并返回
+*/
 class CQueue {
     private Stack<Integer> stack1;
     private Stack<Integer> stack2;
