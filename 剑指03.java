6737d7566d7fa0e30849257bd98a9b5f38244ad0
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-06 22:31:37 +0800

    Create 剑指03.java

---
+++ 剑指03.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        int[] arr = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            arr[nums[i]]++;
+            if (arr[nums[i]] > 1){ 
+                return nums[i];
+                }
+        }
+        return -1;
+    }
+}
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        int repeat = -1;
+        for (int num : nums) {
+            if (!set.add(num)) { // 未添加返回None
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+}
