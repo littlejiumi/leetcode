02217a614b1d6602e3e7f31537012569e6076301
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-14 20:45:59 +0800

    Update 剑指07.java

--- 剑指07.java
+++ 剑指07.java
@@ -6,20 +6,36 @@
  * }
  */
 class Solution {
-    int[] preorder;
-    HashMap<Integer, Integer> dic = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();//标记中序遍历
+    int[] preorder;//保留的先序遍历，方便递归时依据索引查看先序遍历的值
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         this.preorder = preorder;
-        for(int i = 0; i < inorder.length; i++)
-            dic.put(inorder[i], i);
-        return recur(0, 0, inorder.length - 1);
+        //将中序遍历的值及索引放在map中，方便递归时获取左子树与右子树的数量及其根的索引
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        //三个索引分别为
+        //当前根的的索引
+        //递归树的左边界，即数组左边界
+        //递归树的右边界，即数组右边界
+        return recur(0,0,inorder.length-1);
     }
-    TreeNode recur(int root, int left, int right) {
-        if(left > right) return null;                          // 递归终止
-        TreeNode node = new TreeNode(preorder[root]);          // 建立根节点
-        int i = dic.get(preorder[root]);                       // 划分根节点、左子树、右子树
-        node.left = recur(root + 1, left, i - 1);              // 开启左子树递归
-        node.right = recur(root + i - left + 1, i + 1, right); // 开启右子树递归
-        return node;                                           // 回溯返回根节点
+
+    TreeNode recur(int pre_root, int in_left, int in_right){
+        if(in_left > in_right) return null;// 相等的话就是自己
+        TreeNode root = new TreeNode(preorder[pre_root]);//获取root节点
+        int idx = map.get(preorder[pre_root]);//获取在中序遍历中根节点所在索引，以方便获取左子树的数量
+        //左子树的根的索引为先序中的根节点+1 
+        //递归左子树的左边界为原来的中序in_left
+        //递归右子树的右边界为中序中的根节点索引-1
+        root.left = recur(pre_root+1, in_left, idx-1);
+        //右子树的根的索引为先序中的 当前根位置 + 左子树的数量 + 1
+        //递归右子树的左边界为中序中当前根节点+1
+        //递归右子树的有边界为中序中原来右子树的边界
+        root.right = recur(pre_root + (idx - in_left) + 1, idx+1, in_right);
+        return root;
+
     }
+
 }
