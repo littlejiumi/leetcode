289ecae218b0d705cc25eaba5a7085f268d66fcb
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-16 16:22:37 +0800

    Update 剑指12.java

--- 剑指12.java
+++ 剑指12.java
@@ -20,3 +20,36 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        char[] chars = word.toCharArray();
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, chars, visited, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, char[] chars, boolean[][] visited, int i, int j, int start) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length 
+                || chars[start] != board[i][j] || visited[i][j]) {
+            return false;
+        }
+        if (start == chars.length - 1) {
+            return true;    
+        }
+        visited[i][j] = true;
+        boolean ans = false;
+        ans = dfs(board, chars, visited, i + 1, j, start + 1) 
+           || dfs(board, chars, visited, i - 1, j, start + 1)
+           || dfs(board, chars, visited, i, j + 1, start + 1)
+           || dfs(board, chars, visited, i, j - 1, start + 1);
+        visited[i][j] = false;
+        return ans;
+    }
+}
