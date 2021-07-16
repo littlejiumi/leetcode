3216018782962751e54273da9a89b58e73ac4fe9
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-16 11:03:17 +0800

    Update 剑指12.java

--- 剑指12.java
+++ 剑指12.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         char[] words = word.toCharArray();
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                if(dfs(board, words, i, j, 0)) return true;
+        for(int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if (dfs(board, words, i, j, 0)) return true;
             }
         }
         return false;
     }
-    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
-        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
-        if(k == word.length - 1) return true;
+    boolean dfs(char[][] board, char[] words, int i, int j, int k){
+        if(i<0 || i > board.length-1 || j < 0 || j > board[0].length-1 || board[i][j] != words[k]){
+            return false;
+        }
+        if( k == words.length-1) return true;
         board[i][j] = '\0';
-        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) || 
-                      dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
-        board[i][j] = word[k];
-        return res;
+        boolean result = dfs(board, words, i+1, j, k+1) || dfs(board, words, i-1, j, k+1) ||
+                         dfs(board, words, i, j+1, k+1) || dfs(board, words, i, j-1, k+1);
+        board[i][j] = words[k];
+        return result;
     }
 }
