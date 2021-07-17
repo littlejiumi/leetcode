cc98b812716f388bcc55748816f036745a20264f
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-17 16:27:04 +0800

    Update 剑指13.java

--- 剑指13.java
+++ 剑指13.java
@@ -1,9 +1,6 @@
 class Solution {
-    // 棋盘的行列
     int m, n;
-    // 记录位置是否被遍历过
     boolean[][] visited;
-
     public int movingCount(int m, int n, int k) {
         this.m = m;
         this.n = n;
@@ -11,27 +8,20 @@ class Solution {
         return dfs(0, 0, k);
     }
 
-    private int dfs(int i, int j, int k) {
-        if (i >= m || j >= n
-                || visited[i][j] == true
-                || sum(i, j) > k) {
+    private int dfs(int i, int j, int k){
+        if(i>=m || j >= n || visited[i][j] == true || sumNum(i) + sumNum(j) > k){
             return 0;
         }
         visited[i][j] = true;
-        return 1 + dfs(i + 1, j, k)
-                + dfs(i, j + 1, k);
+        return 1 + dfs(i+1, j, k) + dfs(i, j+1, k);
     }
 
-    private int sum(int i, int j) {
-        int sum = 0;
-        while (i != 0) {
-            sum += i % 10;
-            i /= 10;
+    private int sumNum(int num){
+        int ans = 0;
+        while(num != 0){
+            ans += num % 10;
+            num = num / 10;
         }
-        while (j != 0) {
-            sum += j % 10;
-            j /= 10;
-        }
-        return sum;
+        return ans;
     }
 }
