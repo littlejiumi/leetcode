816f913de1f01079f62b56e501084f2d2895627e
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-22 15:06:59 +0800

    Create 剑指26.py

---
+++ 剑指26.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+        def recur(A, B):
+            if not B: return True
+            if not A or A.val != B.val: return False
+            return recur(A.left, B.left) and recur(A.right, B.right)
+        if not A or not B: return False
+        if recur(A, B): return True
+        else: return self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
+
+
