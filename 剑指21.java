e131742bb72a6c8955fe9694f5ad59c3274d9e8a
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-23 10:27:59 +0800

    Update 剑指21.java

--- 剑指21.java
+++ 剑指21.java
@@ -8,3 +8,18 @@ class Solution:
             else:
                 even.append(num)
         return odd + even
+        
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            while left <= right and nums[left] % 2 == 1:
+                left += 1
+            while left <= right and nums[right] % 2 == 0:
+                right -= 1
+            if left > right:
+                break
+            nums[left], nums[right] = nums[right], nums[left]
+        return nums
+
