7083e0f8a16eeeadef31dca932710966ce2de897
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-23 10:53:44 +0800

    Create 剑指21.java

---
+++ 剑指21.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] exchange(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right){
+            while(left <= right && nums[left] % 2 == 1){
+                left++;
+            } 
+            while(left <= right && nums[right] % 2 == 0){
+                right--;
+            }
+            if(left > right) break;
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+        }
+        return nums;
+    }
+}
