c5185b20b07c158e5974516e4d63aa01748af2c2
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-23 10:36:28 +0800

    Update 剑指21.java

--- 剑指21.java
+++ 剑指21.java
@@ -22,4 +22,14 @@ class Solution:
                 break
             nums[left], nums[right] = nums[right], nums[left]
         return nums
+     
+ class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        slow = fast = 0
+        while fast < len(nums):
+            if nums[fast] % 2 == 1:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow +=1
+            fast += 1
+        return nums
 
