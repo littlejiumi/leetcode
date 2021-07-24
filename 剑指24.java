f9dfed7cbd3d7aa75f8533775d94f52ea70d37b9
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-24 11:34:25 +0800

    Create 剑指24.java

---
+++ 剑指24.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while(cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+        return pre;
+    }
+}
