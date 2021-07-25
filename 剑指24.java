ebcc1dd14a671b2140478da9766292259be4aa57
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-25 10:56:28 +0800

    Update 剑指24.java

--- 剑指24.java
+++ 剑指24.java
@@ -10,3 +10,14 @@ class Solution {
         return pre;
     }
 }
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return recur(head, null);    // 调用递归并返回
+    }
+    private ListNode recur(ListNode cur, ListNode pre) {
+        if (cur == null) return pre; // 终止条件
+        ListNode res = recur(cur.next, cur);  // 递归后继节点
+        cur.next = pre;              // 修改节点引用指向
+        return res;                  // 返回反转链表的头节点
+    }
+}
