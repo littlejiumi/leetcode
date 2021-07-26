435b76797f99bd5007425d2c25b225ce01043181
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-07-26 16:23:48 +0800

    Create 剑指32-1.java

---
+++ 剑指32-1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+        if(root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        ArrayList<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            result.add(node.val);
+            if(node.left != null) queue.add(node.left);
+            if(node.right != null) queue.add(node.right);
+        }
+        int[] res = new int[result.size()];
+        for(int i = 0; i < result.size(); i++){
+            res[i] = result.get(i);
+        }
+        return res;
+    }
+}
