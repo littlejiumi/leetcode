0e9a44b2cddd5bdd3c88fedb2c31d67bbdf76eaa
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-05 16:25:55 +0800

    Create 剑指45.py

---
+++ 剑指45.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minNumber(self, nums: List[int]) -> str:
+        def sort_rule(x, y):
+            a, b = x+y, y+x
+            if a>b: return 1
+            elif a < b : return -1
+            else: return 0
+        strs = [str(num) for num in nums]
+        strs.sort(key = functools.cmp_to_key(sort_rule))
+        return ''.join(strs)
