fbc4190a8edd60b40514626a12a3757f3ff3859c
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-18 09:52:02 +0800

    Create 剑指58.py

---
+++ 剑指58.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        return s[n:] + s[:n]
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = []
+        for i in range(n, len(s)):
+            res.append(s[i])
+        for i in range(n):
+            res.append(s[i])
+        return ''.join(res)
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = ""
+        for i in range(n, len(s)):
+            res += s[i]
+        for i in range(n):
+            res += s[i]
+        return res
+
