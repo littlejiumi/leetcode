909f46f509b9b7457c489f862f81a0fb37c3cbcd
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-21 22:15:33 +0800

    Update 剑指04.java

--- 剑指04.java
+++ 剑指04.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
-        int i = matrix.length - 1, j = 0;
+        int i = matrix.length - 1, j = 0;  //从左下角开始，不能右上角，因为matrix[0]有可能为空，不能得到length
         while(i>=0 && j < matrix[0].length){
             if (matrix[i][j] > target){i--;}
             else if (matrix[i][j] < target){j++;}
