c469eb76ba458d1b665cedeba9848351ad2ab592
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-22 11:48:08 +0800

    Update 剑指50.java

--- 剑指50.java
+++ 剑指50.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public char firstUniqChar(String s) {
+        HashMap<Character, Boolean> map = new HashMap<>();
+        char[] charString = s.toCharArray();
+        for(char c : charString){
+            map.put(c, !map.containsKey(c));
+        }
+        for(char c : charString){
+            if(map.get(c)) return c;
+        }
+        return ' ';
+    }
+}
