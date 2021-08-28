0d9a8a28558aa49f78b225fa6165b5d89ac63a60
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-28 15:02:54 +0800

    Create 剑指46.py

---
+++ 剑指46.py
@@ -0,0 +1,10 @@
+class Solution:
+    def translateNum(self, num: int) -> int:
+        s = str(num)
+        a = b= 1
+        for i in range(2, len(s)+1):
+            tmp = s[i-2:i]
+            c = a + b if "10" <= tmp <= "25" else b
+            a = b
+            b = c
+        return b
