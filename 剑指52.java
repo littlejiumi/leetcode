f884ff90ca7ea5b11be5cab043226eb81c6ccd9e
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-28 21:06:16 +0800

    Create 剑指52.java

---
+++ 剑指52.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
