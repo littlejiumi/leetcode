109731b85f9af67d7d68a5469cfe47e1e88c51a4
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-28 16:26:47 +0800

    Update 剑指18.py

--- 剑指18.py
+++ 剑指18.py
@@ -5,11 +5,11 @@
 #         self.next = None
 class Solution:
     def deleteNode(self, head: ListNode, val: int) -> ListNode:
-        if head.val == val: head = head.next
+        if head.val == val: head = head.next #正好是头节点
         cur = head.next
         pre = head
         while cur != None and cur.val != val:
             pre = cur
             cur = cur.next
-        if cur: pre.next = cur.next
+        if cur: pre.next = cur.next # 没有这个节点时，cur==None
         return head
