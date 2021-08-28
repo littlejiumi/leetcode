7236c6aa25898b32a9bb97b31696c3cf972f1309
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-28 21:48:27 +0800

    Update 剑指58-1.py

--- 剑指58-1.py
+++ 剑指58-1.py
@@ -4,3 +4,15 @@ class Solution:
         strs = s.split() # 分割字符串
         strs.reverse() # 翻转单词列表
         return ' '.join(strs) # 拼接为字符串并返回
+    
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip() # 删除首尾空格
+        i = j = len(s) - 1
+        res = []
+        while i >= 0:
+            while i >= 0 and s[i] != ' ': i -= 1 # 搜索首个空格
+            res.append(s[i + 1: j + 1]) # 添加单词
+            while s[i] == ' ': i -= 1 # 跳过单词间空格
+            j = i # j 指向下个单词的尾字符
+        return ' '.join(res) # 拼接并返回
