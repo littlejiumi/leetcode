af1bef976d7d54045dc7078a0a37c85dcfe01ab4
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-08-30 21:12:44 +0800

    Update 剑指36.java

--- 剑指36.java
+++ 剑指36.java
@@ -18,9 +18,9 @@ class Solution {
         //pre用于记录双向链表中位于cur左侧的节点，即上一次迭代中的cur,当pre==null时，cur左侧没有节点,即此时cur为双向链表中的头节点
         if(pre==null) head = cur;
         //反之，pre!=null时，cur左侧存在节点pre，需要进行pre.right=cur的操作。
-        else pre.right = cur;
+        else {pre.right = cur;
        
-        cur.left = pre;//pre是否为null对这句没有影响,且这句放在上面两句if else之前也是可以的。
+        cur.left = pre;}//pre是否为null对这句没有影响,且这句放在上面两句if else之前也是可以的。
 
         pre = cur;//pre指向当前的cur
         dfs(cur.right);//全部迭代完成后，pre指向双向链表中的尾节点
