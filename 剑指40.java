668f502ed66e6e41ad09c67fe61428ade23c02a5
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-01 09:35:06 +0800

    Update 剑指40.java

--- 剑指40.java
+++ 剑指40.java
@@ -8,3 +8,30 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        int[] vec = new int[k];
+        if (k == 0) { // 排除 0 的情况
+            return vec;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;//从大到小，即大根堆
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            queue.offer(arr[i]);
+        }
+        for (int i = k; i < arr.length; ++i) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = queue.poll();
+        }
+        return vec;
+    }
+}
