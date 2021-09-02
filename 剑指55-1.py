1f976f6bf21768f37834866f08326e84cf086dc7
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-02 21:50:16 +0800

    Update and rename 剑指51-1.py to 剑指55-1.py

---
+++ 剑指55-1.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1 
+    
+    
+ class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        queue, res = [root], 0
+        while queue:
+            tmp = []
+            for node in queue:
+                if node.left: tmp.append(node.left)
+                if node.right: tmp.append(node.right)
+            queue = tmp
+            res += 1
+        return res
+
