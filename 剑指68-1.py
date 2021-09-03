c95fd206dbfdeab5cdcc433671f64bc13d987389
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-03 23:18:00 +0800

    Update 剑指68-1.py

--- 剑指68-1.py
+++ 剑指68-1.py
@@ -7,3 +7,11 @@ class Solution:
                 root = root.left # 遍历至左子节点
             else: break
         return root
+
+    class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
