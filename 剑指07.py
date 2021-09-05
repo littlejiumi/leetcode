8c8f78bf977a61d3fd5c0741da5d5a52e763c188
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-05 08:41:24 +0800

    Update 剑指07.py

--- 剑指07.py
+++ 剑指07.py
@@ -1,14 +1,14 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         def recur(root, left, right): #根节点在前序遍历的索引 root 、子树在中序遍历的左边界 left 、子树在中序遍历的右边界 right
-            if left > right: return                               # 递归终止
+            if left > right: return                               # 递归终止条件： 当 left > right ，代表已经越过叶节点，此时返回 null
             node = TreeNode(preorder[root])                       # 建立根节点
             i = dic[preorder[root]]                               # 划分根节点、左子树、右子树
             node.left = recur(root + 1, left, i - 1)              # 开启左子树递归
             node.right = recur(i - left + root + 1, i + 1, right) # 开启右子树递归
             return node                                           # 回溯返回根节点
 
-        dic, preorder = {}, preorder
+        dic = {}
         for i in range(len(inorder)):
             dic[inorder[i]] = i
         return recur(0, 0, len(inorder) - 1)
