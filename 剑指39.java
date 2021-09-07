52b9fec6d7ddfe7b44c012a56545a7b38becf980
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-07 18:25:42 +0800

    Update 剑指39.java

--- 剑指39.java
+++ 剑指39.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {   //哈希表
     public int majorityElement(int[] nums) {
         int bound = nums.length / 2;
         Map<Integer, Integer> counts = new HashMap<>();
@@ -11,5 +11,12 @@ class Solution {
             if (counts.get(num) > nums.length / 2) return num;
         }
         return 0;
+    }
+}
+
+class Solution {  // 排序
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
