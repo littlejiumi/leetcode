730d59936bc104d665f383a04fe7506b0dc74d0c
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-09 09:16:57 +0800

    Update 剑指57-2.java

--- 剑指57-2.java
+++ 剑指57-2.java
@@ -1,4 +1,53 @@
 class Solution {
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> vec = new ArrayList<int[]>();
+        int sum = 0, limit = (target - 1) / 2; // (target - 1) / 2 等效于 target / 2 下取整
+        for (int i = 1; i <= limit; ++i) {
+            for (int j = i;; ++j) {
+                sum += j;
+                if (sum > target) {
+                    sum = 0;
+                    break;
+                } else if (sum == target) {
+                    int[] res = new int[j - i + 1];
+                    for (int k = i; k <= j; ++k) {
+                        res[k - i] = k;
+                    }
+                    vec.add(res);
+                    sum = 0;
+                    break;
+                }
+            }
+        }
+        return vec.toArray(new int[vec.size()][]);
+    }
+}
+
+class Solution {
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> vec = new ArrayList<int[]>();
+        for (int l = 1, r = 2; l < r;) {
+            int sum = (l + r) * (r - l + 1) / 2;
+            if (sum == target) {
+                int[] res = new int[r - l + 1];
+                for (int i = l; i <= r; ++i) {
+                    res[i - l] = i;
+                }
+                vec.add(res);
+                l++;
+            } else if (sum < target) {
+                r++;
+            } else {
+                l++;
+            }
+        }
+        return vec.toArray(new int[vec.size()][]);
+    }
+}
+
+
+
+class Solution {
 public:
     vector<vector<int>> findContinuousSequence(int target) {
         vector<vector<int>> vec;
