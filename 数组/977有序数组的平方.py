f960854a3ac026768bdfb3f505f4230e5c7d79f7
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-16 21:00:32 +0800

    Update 977有序数组的平方.py

--- 数组/977有序数组的平方.py
+++ 数组/977有序数组的平方.py
@@ -1,6 +1,34 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        negative = -1
+        for i, num in enumerate(nums):
+            if num < 0:
+                negative = i
+            else:
+                break
+
+        ans = list()
+        i, j = negative, negative + 1   # 从中间开始，较小的放入ans
+        while i >= 0 or j < n:
+            if i < 0:
+                ans.append(nums[j] * nums[j])
+                j += 1
+            elif j == n:
+                ans.append(nums[i] * nums[i])
+                i -= 1
+            elif nums[i] * nums[i] < nums[j] * nums[j]:
+                ans.append(nums[i] * nums[i])
+                i -= 1
+            else:
+                ans.append(nums[j] * nums[j])
+                j += 1
+
+        return ans
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
         ans = [0] * n
         
         i, j, pos = 0, n - 1, n - 1  # ans从最后一个n-1开始放
