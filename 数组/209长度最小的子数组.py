2bb38cf0e40f614345a5e8088b46224f747a94a0
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-17 17:01:50 +0800

    Update 209长度最小的子数组.py

--- 数组/209长度最小的子数组.py
+++ 数组/209长度最小的子数组.py
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {  //暴力
     public int minSubArrayLen(int target, int[] nums) {
         int n = nums.length;
         if(n==0) return 0;
@@ -14,5 +14,25 @@ class Solution {
             }
         }
         return ans==n+1?0:ans;
+    }
+}
+
+
+class Solution { //滑动窗口
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        if(len == 0) return 0;
+        int l =0, r = 0, sum = 0;
+        int ans = Integer.MAX_VALUE;
+        while(r < len){
+            sum += nums[r];
+            while(sum >= target){
+                ans = Math.min(ans, r - l + 1);
+                sum -= nums[l];
+                l++;
+            }
+            r++;
+        }
+        return ans == Integer.MAX_VALUE? 0: ans;
     }
 }
