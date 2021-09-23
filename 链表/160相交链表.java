4e6ae9322d15a3f47c9978c2ed7921de46ee8e9d
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-23 20:29:03 +0800

    Update 160相交链表.java

--- 链表/160相交链表.java
+++ 链表/160相交链表.java
@@ -52,3 +52,34 @@ public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         return null;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
