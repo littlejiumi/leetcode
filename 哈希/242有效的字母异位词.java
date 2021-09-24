0635e237bd6d1c2acc00697a3fd728335a4d667b
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-24 09:44:22 +0800

    Update 242有效的字母异位词.java

--- 哈希/242有效的字母异位词.java
+++ 哈希/242有效的字母异位词.java
@@ -1,4 +1,4 @@
-class Solution { // 
+class Solution { // sort
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
@@ -10,3 +10,24 @@ class Solution { //
         return Arrays.equals(str1, str2);
     }
 }
+
+
+class Solution {   //哈希法
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {  //长度要相同，不相同直接false
+            return false;
+        }
+        int[] table = new int[26];   // 哈希表
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
