fcf5ccbd776296f110694cd40403180f9b9e491b
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-26 10:58:37 +0800

    Update 349两个数组的交集.java

--- 哈希/349两个数组的交集.java
+++ 哈希/349两个数组的交集.java
@@ -21,3 +21,8 @@ class Solution {
         return result;
     }
 }
+
+#python
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
