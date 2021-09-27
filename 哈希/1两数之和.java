f630aa7579e6258572c549cec480e1ba0b4943f9
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-27 20:19:28 +0800

    Update 1两数之和.java

--- 哈希/1两数之和.java
+++ 哈希/1两数之和.java
@@ -10,3 +10,13 @@ class Solution {
         return new int[0];
     }
 }
+#python:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
+
