0867baae74271c8e3b0adec9339e5f0684dbe4b8
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-29 18:29:02 +0800

    Create 225用队列实现栈.java

---
+++ 栈和队列/225用队列实现栈.java
@@ -0,0 +1,42 @@
+class MyStack {
+public:
+    queue<int> que1;
+    queue<int> que2; // 辅助队列，用来备份
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        que1.push(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int size = que1.size();
+        size--;
+        while (size--) { // 将que1 导入que2，但要留下最后一个元素
+            que2.push(que1.front());
+            que1.pop();
+        }
+
+        int result = que1.front(); // 留下的最后一个元素就是要返回的值
+        que1.pop();
+        que1 = que2;            // 再将que2赋值给que1
+        while (!que2.empty()) { // 清空que2
+            que2.pop();
+        }
+        return result;
+    }
+
+    /** Get the top element. */
+    int top() {
+        return que1.back();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return que1.empty();
+    }
+};
