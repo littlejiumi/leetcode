10fa2b47bb6dc223f525cf5b227b279d70a7d3fa
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-09-29 18:19:56 +0800

    Update 232用栈实现队列.java

--- 栈和队列/232用栈实现队列.java
+++ 栈和队列/232用栈实现队列.java
@@ -1,39 +1,53 @@
 import java.util.Stack;
 
 class MyQueue {
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
-    private boolean isPushState = true;
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
     /** Initialize your data structure here. */
     public MyQueue() {
-      // 输入栈
-      stack1 = new Stack<Integer>();
-      // 输出栈
-      stack2 = new Stack<Integer>();
+        stack1 = new Stack<>(); // 负责进栈
+        stack2 = new Stack<>(); // 负责出栈
     }
-
+    
     /** Push element x to the back of queue. */
     public void push(int x) {
-      stack1.push(x);
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {    
+        dumpStack1();
+        return stack2.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        dumpStack1();
+        return stack2.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 
-    public void transformStack () {
-        if (stack2.empty()) {
-            while (!stack1.empty()) {
-                int t = stack1.pop();
-                stack2.push(t);
+    // 如果stack2为空，那么将stack1中的元素全部放到stack2中
+    private void dumpStack1(){
+        if (stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                stack2.push(stack1.pop());
             }
         }
     }
-
-    /** Removes the element from in front of queue and returns that element. */
-    public int pop() {
-        transformStack();
-        return stack2.pop();
-    }
-
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-      return stack1.empty() && stack2.empty();
-    }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
