3b3764a1f71e31039478faf4058cae0ec180d6c0
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-09 15:44:58 +0800

    Create 151翻转字符串里的单词.java

---
+++ 字符串/151翻转字符串里的单词.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+}
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
