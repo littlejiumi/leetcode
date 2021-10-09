531c0db8c16a1a0997f2fe68b80e5817192ec9f9
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-09 21:14:47 +0800

    Update 排序算法.java

--- 排序算法.java
+++ 排序算法.java
@@ -15,4 +15,29 @@ public class BubbleSort { // 冒泡排序 1、时间复杂度：O(n2) 2、空间
         }
         return arr;
     }
-)
+}
+// 假如从开始的第一对到结尾的最后一对，相邻的元素之间都没有发生交换的操作，这意味着右边的元素总是大于等于左边的元素，此时的数组已经是有序的了，我们无需再对剩余的元素重复比较下去了。
+public class BubbleSort {
+    public static int[] bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return arr;
+        }
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < n -i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    flag = false;
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+            }
+            //一趟下来是否发生位置交换
+            if(false)
+                break;
+        }
+        return arr;
+    }
+}
+
