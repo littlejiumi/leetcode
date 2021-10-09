25a2090224d0d1716fc27328df651b9253f41b34
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-09 16:45:39 +0800

    Update 347前 K 个高频元素.java

--- 栈和队列/347前 K 个高频元素.java
+++ 栈和队列/347前 K 个高频元素.java
@@ -1,23 +1,35 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        int[] result = new int[k];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
+        HashMap<Integer,Integer> map = new HashMap();
+        for(int num : nums){
+            if (map.containsKey(num)) {
+               map.put(num, map.get(num) + 1);
+             } else {
+                map.put(num, 1);
+             }
         }
-
-        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
-        // 根据map的value值正序排，相当于一个小顶堆
-        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
-        for (Map.Entry<Integer, Integer> entry : entries) {
-            queue.offer(entry);
-            if (queue.size() > k) {
-                queue.poll();
+        // 遍历map，用最小堆保存频率最大的k个元素
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(a) - map.get(b);
+            }
+        });
+        for (Integer key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.add(key);
+            } else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove();
+                pq.add(key);
             }
         }
-        for (int i = k - 1; i >= 0; i--) {
-            result[i] = queue.poll().getKey();
+        // 取出最小堆中的元素
+        List<Integer> res = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            res.add(pq.remove());
         }
-        return result;
+        return res;
     }
 }
+
