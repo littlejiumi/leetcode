db12d8a03381cbecf1d90f99a07ab135acaf9102
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-10 11:43:22 +0800

    Update 347前 K 个高频元素.java

--- 栈和队列/347前 K 个高频元素.java
+++ 栈和队列/347前 K 个高频元素.java
@@ -5,11 +5,9 @@ class Solution {
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
         // 根据map的value值正序排，相当于一个小顶堆
         PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
-        for (Map.Entry<Integer, Integer> entry : entries) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             queue.offer(entry);
             if (queue.size() > k) {
                 queue.poll();
