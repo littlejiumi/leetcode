5b9489b47ce09e244cfa58d5dd25b3795645ea15
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-10 21:09:39 +0800

    Update 226.翻转二叉树.java

--- 二叉树/226.翻转二叉树.java
+++ 二叉树/226.翻转二叉树.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public TreeNode invertTree(TreeNode root) {  // 前序遍历
+    public TreeNode invertTree(TreeNode root) {  // 前序遍历，遍历的过程中去翻转每一个节点的左右孩子就可以达到整体翻转的效果。
         if(root == null) return null;
         invertTree(root.left);
         invertTree(root.right);
