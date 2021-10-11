c856cb977f3818519be817b7b96f6d4f2c4cba27
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-11 10:09:32 +0800

    Create 111二叉树的最小深度.java

---
+++ 二叉树/111二叉树的最小深度.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int getDepth(TreeNode node) {
+        if (node == null) return 0; // 终止条件
+        int leftDepth = getDepth(node.left); // 左子树
+        int rightDepth = getDepth(node.right);  // 右子树
+        //最小深度是从根节点到最近叶子节点的最短路径上的节点数量。注意是叶子节点。
+        // 如果左子树为空，右子树不为空，说明最小深度是 1 + 右子树的深度。
+        if (node.left == null && node.right != null) {
+            return 1 + rightDepth;
+        }
+        // 右子树为空，左子树不为空，最小深度是 1 + 左子树的深度。 
+        else if (node.left !=null && node.right == null) {
+            return 1 + leftDepth;
+        }
+        // 如果左右子树都不为空或都为空，返回左右子树深度最小值 + 1 。
+        int result = 1 + Math.min(leftDepth, rightDepth);
+        return result;
+    }
+    public int minDepth(TreeNode root) {
+        return getDepth(root);
+    }
+}
