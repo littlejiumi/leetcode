99ca152e3fc016fac757f93ca80d4ed1d8babd9c
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-12 22:24:46 +0800

    Create 剑指39数组中出现次数超过一半的数字.java

---
+++ map&set/剑指39数组中出现次数超过一半的数字.java
@@ -0,0 +1,39 @@
+class Solution {
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            } else {
+                counts.put(num, counts.get(num) + 1);
+            }
+        }
+        return counts;
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+}
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        return nums[len/2];
+    }
+}
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = collections.Counter(nums)
+        return counts.most_common()[0][0];
