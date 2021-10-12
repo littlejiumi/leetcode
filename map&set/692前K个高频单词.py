e297869995aa6ae7ae41cc653cf0ad77348d4f40
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-12 19:17:20 +0800

    Update 692前K个高频单词.py

--- map&set/692前K个高频单词.py
+++ map&set/692前K个高频单词.py
@@ -2,5 +2,9 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:        
         hash = collections.Counter(words)
-        res = sorted(hash, key=lambda word:(-hash[word], word))       
+         # sorted 方法默认正序排列,第一个参数-hash[word] 即单词出现次数的相反数;当词频相同时，用第二个参数 word 进行排序，即字母正序排列
+        res = sorted(hash, key=lambda word:(-hash[word], word)) 
+        # sorted(hash, key=lambda word:(hash[word], word)) 词频正序， 字母正序
+        # sorted(hash, key=lambda word:(hash[word], word), reverse=True) 词频倒序， 字母倒序 （reverse=True 即将sorted方法修改为倒序排列）
+        # sorted(hash, key=lambda word:(-hash[word], word), reverse=True) 词频正序， 字母倒序
         return res[:k]
