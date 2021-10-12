180506c7d77d094d71b77485a8598f83e253a568
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-12 19:06:54 +0800

    Update 692前K个高频单词.py

--- map&set/692前K个高频单词.py
+++ map&set/692前K个高频单词.py
@@ -1,3 +1,4 @@
+# 给一非空的单词列表，返回前 k 个出现次数最多的单词。返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:        
         hash = collections.Counter(words)
