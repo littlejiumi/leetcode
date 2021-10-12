94d8e4ba9513eb92088d36ad0f98f15808bfd528
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-12 21:24:54 +0800

    Update 692前K个高频单词.java

--- map&set/692前K个高频单词.java
+++ map&set/692前K个高频单词.java
@@ -1,3 +1,4 @@
+// ArrayList sort
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         //存放一个map中
@@ -17,5 +18,32 @@ class Solution {
             }
         });
         return rec.subList(0, k);
+    }
+}
+
+// 优先队列
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        List<String> ret = new ArrayList<String>();
+        while (!pq.isEmpty()) {
+            ret.add(pq.poll().getKey());
+        }
+        Collections.reverse(ret);
+        return ret;
     }
 }
