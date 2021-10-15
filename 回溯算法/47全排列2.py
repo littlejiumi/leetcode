900745a73207e1e2701b3a646555f7611c65c5e3
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-15 16:38:39 +0800

    Create 47全排列2.py

---
+++ 回溯算法/47全排列2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res, path, used = [], [], []
+        def backtrack(nums, used):
+            if len(nums) == len(path):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if i > 0 and nums[i-1]==nums[i] and (i-1 not in used):
+                    continue
+                if i in used: continue
+                path.append(nums[i])
+                used.append(i)   # 注意，不能放nums[i]，因为有重复的元素。存放index是唯一的。
+                backtrack(nums, used)
+                used.pop()
+                path.pop()
+        nums.sort()
+        backtrack(nums, used)
+        return res
