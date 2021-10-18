444354c04ff0ff53f8eefb76cf0a96b6876e59c7
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-18 09:35:00 +0800

    Update 343整数拆分.java

--- 动态规划/343整数拆分.java
+++ 动态规划/343整数拆分.java
@@ -1,6 +1,7 @@
 class Solution {
     public int integerBreak(int n) {
         int[] dp = new int[n+1];
+        dp[1] = 0;
         dp[2] = 1;
         for(int i = 3; i <= n; i++) {
             for(int j = 1; j <= i-1; j++){
