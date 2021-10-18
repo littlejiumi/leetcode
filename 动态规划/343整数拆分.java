667187b6e30b9fff99ddf1328e38b19c5badd049
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-18 09:33:57 +0800

    Update 343整数拆分.java

--- 动态规划/343整数拆分.java
+++ 动态规划/343整数拆分.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[n+1];
         dp[2] = 1;
         for(int i = 3; i <= n; i++) {
-            for(int j = 1; j < i-1; j++){
+            for(int j = 1; j <= i-1; j++){
                 // 每次计算dp[i]，取最大的
                 // j * (i - j) 是单纯的把整数拆分为两个数相乘，而j * dp[i - j]是拆分成两个以及两个以上的个数相乘
                 dp[i] = Math.max(dp[i], Math.max(dp[i-j]*j, j * (i-j)));
