79ecd22482cf1fa98f25e621cd13fb2cbcbb9eeb
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-22 16:31:31 +0800

    Update 238除自身以外数组的乘积.java

--- 数组/238除自身以外数组的乘积.java
+++ 数组/238除自身以外数组的乘积.java
@@ -1,15 +1,15 @@
-class Solution { //乘积 = 当前数左边的乘积 * 当前数右边的乘积
+class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] res = new int[nums.length];
         int k = 1;
-        for(int i = 0; i < res.length; i++){
+        for(int i = 0; i < nums.length; i++) {
             res[i] = k;
-            k = k * nums[i]; // 此时数组存储的是除去当前元素左边的元素乘积
+            k *= nums[i];
         }
         k = 1;
-        for(int i = res.length - 1; i >= 0; i--){
-            res[i] *= k; // k为该数右边的乘积。
-            k *= nums[i]; // 此时数组等于左边的 * 该数右边的。
+        for(int i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * k;
+            k *= nums[i];
         }
         return res;
     }
