f3e6ee04e592cc88ba41fcd570e6a6dcbd338039
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-25 19:30:15 +0800

    Create 98. 验证二叉搜索树.py

---
+++ 二叉树/98. 验证二叉搜索树.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        res=[]
+        def inorder(root):
+            if not root: return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        last = res[0]
+        for i in range(1, len(res)):
+            if res[i] <= last:
+                return False
+            last = res[i]
+        return True
