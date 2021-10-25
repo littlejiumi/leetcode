76684d55006490b4054b471bdff97d20011dd1b2
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-25 19:55:22 +0800

    Update 98. 验证二叉搜索树.py

--- 二叉树/98. 验证二叉搜索树.py
+++ 二叉树/98. 验证二叉搜索树.py
@@ -19,3 +19,20 @@ class Solution:
                 return False
             last = res[i]
         return True
+
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+}
