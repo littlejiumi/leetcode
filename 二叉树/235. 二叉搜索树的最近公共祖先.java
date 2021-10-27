965f0812d584de7bf907dd472520c97d568a0f02
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-27 20:45:48 +0800

    Update 235. 二叉搜索树的最近公共祖先.java

--- 二叉树/235. 二叉搜索树的最近公共祖先.java
+++ 二叉树/235. 二叉搜索树的最近公共祖先.java
@@ -23,3 +23,11 @@ class Solution {
         return root;
     }
 }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //如果小于等于0，说明p和q位于root的两侧，直接返回即可
+        if ((root.val - p.val) * (root.val - q.val) <= 0)
+            return root;
+        //否则，p和q位于root的同一侧，就继续往下找
+        return lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);
+    }
