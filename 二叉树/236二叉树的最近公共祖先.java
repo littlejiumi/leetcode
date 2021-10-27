2692aab1cc5d4fb2dae46869bc9b46fb1a64fbf3
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-27 20:24:43 +0800

    Update 236二叉树的最近公共祖先.java

--- 二叉树/236二叉树的最近公共祖先.java
+++ 二叉树/236二叉树的最近公共祖先.java
@@ -9,12 +9,13 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left == null) return right;
+        if(root == null || root == p || root == q) return root;  
+        // 后序遍历
+        TreeNode left = lowestCommonAncestor(root.left, p, q);  // 在左子树找两个节点
+        TreeNode right = lowestCommonAncestor(root.right, p, q);  // 在右子树找两个节点
+        if(left == null) return right;  // 没找到，则在另一侧子树寻找
         if(right == null) return left;
         // left != NULL && right != NULL
-        return root;
+        return root;   // 返回根节点
     }
 }
