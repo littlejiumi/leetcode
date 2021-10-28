1fece4226880affa264b0f063c3f46418cbef311
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-28 11:11:57 +0800

    Update 27移除元素.java

--- 双指针/27移除元素.java
+++ 双指针/27移除元素.java
@@ -1,15 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int fastIndex = 0;
-        int slowIndex = 0;  
-        for (fastIndex = 0; fastIndex < nums.length; fastIndex++) {
-            if (nums[fastIndex] != val) {
-                nums[slowIndex] = nums[fastIndex];
-                slowIndex++;   // 遇到不是val的情况，快指针给慢指针赋值
+        int left = 0, right = 0;
+        while(right < nums.length){
+            if(nums[right] != val){
+                nums[left] = nums[right];
+                left++;
             }
-            // 遇到是val的情况，就不赋值了，只有快指针向前移动
+            right++;
         }
-        return slowIndex;
-
+        return left;
     }
 }
