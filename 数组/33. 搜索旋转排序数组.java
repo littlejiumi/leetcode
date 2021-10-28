b5a8eed472a8252145b8a8f1b5d69f1e00c6aa68
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-28 15:58:13 +0800

    Update 33. 搜索旋转排序数组.java

--- 数组/33. 搜索旋转排序数组.java
+++ 数组/33. 搜索旋转排序数组.java
@@ -1,29 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        if (n == 0) {
-            return -1;
-        }
-        if (n == 1) {
-            return nums[0] == target ? 0 : -1;
-        }
-        int l = 0, r = n - 1;
-        while (l <= r) {
+        int l = 0, r = nums.length - 1;
+        while(l <= r) {
             int mid = (l + r) / 2;
-            if (nums[mid] == target) {
+            if(nums[mid] == target) {
                 return mid;
             }
-            if (nums[0] <= nums[mid]) {
-                if (nums[0] <= target && target < nums[mid]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
+            if (nums[mid] >= nums[0]){
+                if(target < nums[mid] && nums[0] <= target){
+                    r = mid-1;
+                }else{
+                    l = mid+1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[n - 1]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
+            }else{
+                if(nums[mid] <= target && target <= nums[r]){
+                    l = mid+1;
+                }else{
+                     r = mid-1;
                 }
             }
         }
