f605dbe6f880e08c590601abdfff324f7a5f04d5
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-28 10:17:04 +0800

    Create 16. 最接近的三数之和.java

---
+++ 双指针/16. 最接近的三数之和.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left != right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(Math.abs(sum - target) < Math.abs(result - target))
+                    result = sum;
+                if(sum > target){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
+
