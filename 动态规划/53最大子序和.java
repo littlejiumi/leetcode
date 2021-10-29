f0f50171ee537d8b216c334db3d0ba7d04448084
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-29 20:40:50 +0800

    Update 53最大子序和.java

--- 动态规划/53最大子序和.java
+++ 动态规划/53最大子序和.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int[] dp = new int[nums.length];
+        int[] dp = new int[nums.length]; // 以nums[i]结尾的最大子序和
         dp[0] = nums[0];
         int max = dp[0];
         for(int i = 1; i < nums.length; i++) {
