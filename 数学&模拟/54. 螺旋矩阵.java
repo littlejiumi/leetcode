e8c1affc4b8b3b291b15aced73d529b6e834060c
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-10-30 11:12:30 +0800

    Update 54. 螺旋矩阵.java

--- 数学&模拟/54. 螺旋矩阵.java
+++ 数学&模拟/54. 螺旋矩阵.java
@@ -1,31 +1,28 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int left = 0, right = matrix[0].length, top = 0, blow = matrix.length;
+        int t = 0, b = matrix.length-1, l = 0, r = matrix[0].length-1;
         List<Integer> res = new ArrayList<>();
-        while (true){
-            for (int i = left; i < right; i++){//第一行从左向右
-                res.add(matrix[top][i]);
+        while(true){ // 每一行/列都包括最后一个数写
+            for(int i = l; i <= r; i++) {
+                res.add(matrix[t][i]);
             }
-            top++; //上边界收缩，判断是否结束
-            if (top == blow) return res;
-
-            for (int i = top; i < blow; i++){//最后一列从上至下
-                res.add(matrix[i][right - 1]);
+            if(t == b) return res;  // 先比较是否合上了
+            t++;                    // 再变化
+            for(int i = t; i <= b; i++) {
+                res.add(matrix[i][r]);
             }
-            right--;//右边界收缩，判断是否结束
-            if (right == left) return res;
-
-            for (int i = right - 1; i >= left; i--){//最后一行从右至左
-                res.add(matrix[blow - 1][i]);
+            if(r == l) return res;
+            r--;
+            for(int i = r; i >= l; i--) {
+                res.add(matrix[b][i]);
             }
-            blow--;//下边界收缩，判断是否结束
-            if (blow == top) return res;
-
-            for (int i = blow - 1; i >= top; i--){//第一列从下至上
-                res.add(matrix[i][left]);
+            if(t == b) return res;
+            b--;
+            for(int i = b; i >= t; i--) {
+                res.add(matrix[i][l]);
             }
-            left++;//左边界收缩，判断是否结束
-            if (left == right) return res;
+            if(r == l) return res;
+            l++;
         }
     }
 }
