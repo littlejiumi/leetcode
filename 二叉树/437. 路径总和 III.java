c744a3754d5ac09734c6778afc0410ec4d68657e
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-01 18:25:04 +0800

    Update 437. 路径总和 III.java

--- 二叉树/437. 路径总和 III.java
+++ 二叉树/437. 路径总和 III.java
@@ -1,14 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-public:
-    int pathSum(TreeNode* root, int sum) {
-        if (root == NULL) return 0;
-        return pathWithRoot(root, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    public int dfs(TreeNode node, int count){
+        if(node == null) return 0;
+        count -= node.val;
+        int noderes = (count == 0? 1 : 0);
+        int left = dfs(node.left, count);
+        int right = dfs(node.right, count);
+        return noderes + left + right;
     }
-
-    int pathWithRoot(TreeNode* root, int sum) {
-        if (root == NULL) return 0;
-        sum = sum - root->val;
-        return (sum == 0 ? 1 : 0) + pathWithRoot(root->left, sum) + pathWithRoot(root->right, sum);
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root == null) return 0;
+        return dfs(root, targetSum) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
     }
-};
-
+}
