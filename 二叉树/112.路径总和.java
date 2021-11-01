c1b5ac06e1df615b3d7be33599d5c7d3b1e2ff10
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-01 19:05:13 +0800

    Update 112.路径总和.java

--- 二叉树/112.路径总和.java
+++ 二叉树/112.路径总和.java
@@ -15,18 +15,17 @@
  */
 class Solution {
     public boolean traversal(TreeNode node, int count) {
-        count -= node.val;
         if(count == 0 && node.left== null && node.right == null) return true;
         if(node.left != null) {
-            if (traversal(node.left, count)) return true; 
+            if (traversal(node.left, count-node.left.val)) return true; 
         }
         if(node.right != null) {
-            if (traversal(node.right, count)) return true;
+            if (traversal(node.right, count-node.right.val)) return true;
         }
         return false;
     }
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        return traversal(root, targetSum);
+        return traversal(root, targetSum-root.val);
     }
 }
