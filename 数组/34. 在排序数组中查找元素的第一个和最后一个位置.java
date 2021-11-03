7db4bed9cf0dafa0ac7e9cd180b67adedac33577
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-03 15:35:19 +0800

    Update 34. 在排序数组中查找元素的第一个和最后一个位置.java

--- 数组/34. 在排序数组中查找元素的第一个和最后一个位置.java
+++ 数组/34. 在排序数组中查找元素的第一个和最后一个位置.java
@@ -1,44 +1,45 @@
 class Solution {
-    int[] searchRange(int[] nums, int target) {
-        int leftBorder = getLeftBorder(nums, target);
-        int rightBorder = getRightBorder(nums, target);
-        // 情况一
-        if (leftBorder == -2 || rightBorder == -2) return new int[]{-1, -1};
-        // 情况三
-        if (rightBorder - leftBorder > 1) return new int[]{leftBorder + 1, rightBorder - 1};
-        // 情况二
-        return new int[]{-1, -1};
-    }
-
-    int getRightBorder(int[] nums, int target) {
+/*
+情况一：target 在数组范围的右边或者左边，例如数组{3, 4, 5}，target为2或者数组{3, 4, 5},target为6，此时应该返回{-1, -1}
+情况二：target 在数组范围中，且数组中不存在target，例如数组{3,6,7},target为5，此时应该返回{-1, -1}
+情况三：target 在数组范围中，且数组中存在target，例如数组{3,6,7},target为6，此时应该返回{1, 1} */
+    public int getLeft(int[] nums, int target){
         int left = 0;
-        int right = nums.length - 1;
-        int rightBorder = -2; // 记录一下rightBorder没有被赋值的情况
-        while (left <= right) {
-            int middle = left + ((right - left) / 2);
-            if (nums[middle] > target) {
-                right = middle - 1;
-            } else { // 寻找右边界，nums[middle] == target的时候更新left
-                left = middle + 1;
-                rightBorder = left;
-            }
-        }
-        return rightBorder;
-    }
-
-    int getLeftBorder(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int leftBorder = -2; // 记录一下leftBorder没有被赋值的情况
-        while (left <= right) {
-            int middle = left + ((right - left) / 2);
-            if (nums[middle] >= target) { // 寻找左边界，nums[middle] == target的时候更新right
+        int right = nums.length-1;
+        int leftBorder = -2;
+        while(left <= right) {
+            int middle = left + (right - left) / 2;    
+            if(nums[middle] >=  target){ // 寻找小于target最右侧的值，即左边界-1
                 right = middle - 1;
                 leftBorder = right;
-            } else {
+            }else{
                 left = middle + 1;
             }
         }
         return leftBorder;
+    }
+    // 计算出来的右边界是不包好target的最左值
+    public int getRight(int[] nums, int target){ 
+        int left = 0;
+        int right = nums.length-1;
+        int rightBorder = -2;
+        while(left <= right) {
+            int middle = left + (right - left) / 2;    
+            if(nums[middle] <=  target){
+                left = middle + 1;
+                rightBorder = left;
+            }else{
+                right = middle - 1;
+            }
+        }
+        return rightBorder;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int leftBorder = getLeft(nums, target);
+        int rightBorder = getRight(nums, target);
+        if(leftBorder == -2 || rightBorder == -2) return new int[]{-1, -1};
+        if(rightBorder - leftBorder > 1) return new int[]{leftBorder+1, rightBorder-1};
+        return new int[]{-1,-1};
+
     }
 }
