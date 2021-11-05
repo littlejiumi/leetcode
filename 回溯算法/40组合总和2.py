bc0695c71f00c0762a78730905a37d890295459c
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-05 22:05:26 +0800

    Update 40组合总和2.py

--- 回溯算法/40组合总和2.py
+++ 回溯算法/40组合总和2.py
@@ -1,22 +1,24 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def backtracking(candidates, target, sum, startIndex):
-            if sum > target: return
-            if sum == target:
-                res.append(path[:])  # 重要，别忘啦
-                return 
-            for i in range(startIndex, len(candidates)):
-                if sum + candidates[i] >  target: return # 剪枝：如果 sum + candidates[i] > target 就终止遍历
-                if i > startIndex and candidates[i]==candidates[i-1]:continue  # 同层重复continue
-                sum += candidates[i]
-                path.append(candidates[i])
-                backtracking(candidates, target, sum, i+1)  
-                sum -= candidates[i]
-                path.pop()
-                
         res, path = [], []
-        candidates.sort()
-        backtracking(candidates, target, 0, 0)
+        used = [0] * len(candidates)
+        def backtracking(candidates, target, startIndex, Sum, used):
+            if Sum == target:
+                res.append(path[:]) # 重要，别忘啦
+                return
+            if Sum > target: return
+            for i in range(startIndex, len(candidates)):
+                if i > startIndex and candidates[i] == candidates[i-1] and used[i-1] == 0: # 同层重复continue
+                    continue
+                path.append(candidates[i])
+                Sum += candidates[i]
+                used[i] = 1
+                backtracking(candidates, target, i+1, Sum, used)
+                path.pop()
+                used[i] = 0
+                Sum -= candidates[i]
+        candidates.sort()  # 树层去重的话，需要对数组排序
+        backtracking(candidates, target, 0, 0, used)
         return res
     
     class Solution {
