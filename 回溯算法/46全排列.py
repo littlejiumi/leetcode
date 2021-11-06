7ce7154fe2d2d8a2d2a89d3f7920a31342882580
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-06 12:24:09 +0800

    Update 46全排列.py

--- 回溯算法/46全排列.py
+++ 回溯算法/46全排列.py
@@ -1,16 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res, path, used = [], [], []
-        def backtrack(nums, used):
-            if len(path) == len(nums):
+        res, path = [], []
+        used = [0] * len(nums)
+        def backtracking(nums, used):  # 全排列问题不需要startIndex，但是需要used数组
+            if len(nums) == len(path):
                 res.append(path[:])
-                return
             for i in range(len(nums)):
-                if i in used: continue
+                if used[i] == 1: continue
+                used[i] = 1
                 path.append(nums[i])
-                used.append(i)
-                backtrack(nums, used)
-                used.pop()
+                backtracking(nums, used)
                 path.pop()
-        backtrack(nums, used)
+                used[i] = 0
+        backtracking(nums, used)
         return res
