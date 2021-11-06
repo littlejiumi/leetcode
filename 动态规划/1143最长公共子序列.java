8f333efb64327e3b071ddacd41b8a18c0ff62ef5
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-06 18:39:13 +0800

    Update 1143最长公共子序列.java

--- 动态规划/1143最长公共子序列.java
+++ 动态规划/1143最长公共子序列.java
@@ -15,3 +15,19 @@ class Solution {
         return dp[text1.length()][text2.length()];
     }
 }
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)] # 每次建立的dp数组比字符串规模大1，可以简化边界操作。
+        # dp[i][j]的含义是，text1[0:i-1]和text2[0:j-1]的最长公共子序列的长度,注意是到前一个为止
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1  # 边界操作：-1时为0
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[m][n]
+
