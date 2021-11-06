a99dc2920e36f87c4377be6712ba9af35a6060b4
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-06 11:50:15 +0800

    Update 90子集2.py

--- 回溯算法/90子集2.py
+++ 回溯算法/90子集2.py
@@ -1,14 +1,17 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res, path = [], []
-        def backtrack(nums, startIndex):
+        path, res = [], []
+        used = [0] * len(nums)
+        def backtracking(nums, startIndex, used):
             res.append(path[:])
             for i in range(startIndex, len(nums)):
-                if i > startIndex and nums[i] == nums[i-1]:
+                if i > startIndex and used[i]==0 and nums[i] == nums[i-1]:
                     continue
                 path.append(nums[i])
-                backtrack(nums, i+1)
+                used[i]=1
+                backtracking(nums, i+1, used)
+                used[i] = 0
                 path.pop()
         nums.sort()
-        backtrack(nums, 0)
+        backtracking(nums, 0, used)
         return res
