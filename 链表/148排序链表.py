e03b90aa6c9552526cf05850504f9acdac9c38e5
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-10 09:14:56 +0800

    Update 148排序链表.py

--- 链表/148排序链表.py
+++ 链表/148排序链表.py
@@ -16,3 +16,43 @@ class Solution:
             h = h.next
         h.next = left if left else right
         return res.next
+    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution { // 先找到链表中点，左右分割成两个链表，归并得到两个分别有序的链表，从而进行有序链表的排序
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+}
