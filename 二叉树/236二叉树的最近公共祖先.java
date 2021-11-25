38e64dfe99b73b00fd0e2f409f38669f17676756
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-25 15:06:31 +0800

    Update 236二叉树的最近公共祖先.java

--- 二叉树/236二叉树的最近公共祖先.java
+++ 二叉树/236二叉树的最近公共祖先.java
@@ -19,3 +19,20 @@ class Solution {
         return root;   // 返回根节点
     }
 }
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q: return root # 如果等于p或者q，返回
+        L = self.lowestCommonAncestor(root.left, p, q)  
+        R = self.lowestCommonAncestor(root.right, p, q)
+        if L and R: return root  # 如果root两边分别有p和q，那就是root了，返回
+        if L and not R: return L 
+        if R and not L: return R 
+        return None
