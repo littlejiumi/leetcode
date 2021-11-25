119d13e9b123fc76aff8fc28f7ca4be98a28f5ba
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-25 14:23:04 +0800

    Update 98. 验证二叉搜索树.py

--- 二叉树/98. 验证二叉搜索树.py
+++ 二叉树/98. 验证二叉搜索树.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution:  #用到了二叉搜索树的中序遍历是有序数组这样的性质
     def isValidBST(self, root: TreeNode) -> bool:
         res=[]
         def inorder(root):
@@ -21,18 +21,28 @@ class Solution:
         return True
 
 
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
+# class Solution {
+#     public boolean isValidBST(TreeNode root) {
+#         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+#     }
 
-    public boolean isValidBST(TreeNode node, long lower, long upper) {
-        if (node == null) {
-            return true;
-        }
-        if (node.val <= lower || node.val >= upper) {
-            return false;
-        }
-        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
-    }
-}
+#     public boolean isValidBST(TreeNode node, long lower, long upper) {
+#         if (node == null) {
+#             return true;
+#         }
+#         if (node.val <= lower || node.val >= upper) {
+#             return false;
+#         }
+#         return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+#     }
+# }
+
+class Solution:   # 递归法，中序遍历
+    minNum = -float('inf')
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root: return True
+        is_left_valid = self.isValidBST(root.left)#左
+        if self.minNum < root.val: self.minNum = root.val # 中： 当前最大值比root小才继续，否则返回False
+        else: return False
+        is_right_valid = self.isValidBST(root.right） # 右
+        return is_left_valid and is_right_valid
