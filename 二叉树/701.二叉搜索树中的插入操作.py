12979e48b10a2f33b23479139a805e5d07f9cda8
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-26 10:07:40 +0800

    Create 701.二叉搜索树中的插入操作.py

---
+++ 二叉树/701.二叉搜索树中的插入操作.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            node = TreeNode(val)
+            return node
+        if val < root.val: root.left = self.insertIntoBST(root.left, val)
+        if val > root.val: root.right = self.insertIntoBST(root.right, val)
+        return root
