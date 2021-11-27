d734196930d0dd6273ea27ff5152e71f6a9ae1d4
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-27 14:30:06 +0800

    Create 189. 轮转数组.py

---
+++ 数组/189. 轮转数组.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        if k  == 0 : return
+        R = nums[-k:]
+        L = nums[:-k]
+        nums[:k] = R
+        nums[k:] = L
