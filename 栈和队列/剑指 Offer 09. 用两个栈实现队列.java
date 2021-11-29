817628212184e7806255ef5409bde881019602c1
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-11-29 11:11:41 +0800

    Update 剑指 Offer 09. 用两个栈实现队列.java

--- 栈和队列/剑指 Offer 09. 用两个栈实现队列.java
+++ 栈和队列/剑指 Offer 09. 用两个栈实现队列.java
@@ -42,3 +42,20 @@ class CQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+
+
+class CQueue:
+
+    def __init__(self):
+        self.A, self.B = [], []
+
+    def appendTail(self, value: int) -> None:
+        self.A.append(value)
+
+    def deleteHead(self) -> int:
+        if self.B: return self.B.pop()
+        if not self.A: return -1
+        while self.A:
+            self.B.append(self.A.pop())
+        return self.B.pop()
