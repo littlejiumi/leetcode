1a1e9f0a848bf9f2a6b17b4a08e108a1be7bff0d
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-12-01 11:35:48 +0800

    Update 3. 无重复字符的最长子串.py

--- map&set/3. 无重复字符的最长子串.py
+++ map&set/3. 无重复字符的最长子串.py
@@ -9,3 +9,19 @@ class Solution:  # 滑动窗口
             maxres = max(maxres, i - left + 1)
             c_dict[s[i]] = i
         return maxres
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0, start = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)){
+                start = Math.max(map.get(ch)+1,start);
+            }
+            max = Math.max(max,end - start + 1);
+            map.put(ch,end);
+        }
+        return max;
+    }
+}
