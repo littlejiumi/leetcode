3e9ba8706e2222d7c913e7fb6f819ea3e581e2d1
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-12-15 10:28:47 +0800

    Update 剑指 Offer 11. 旋转数组的最小数字.py

--- 数组/剑指 Offer 11. 旋转数组的最小数字.py
+++ 数组/剑指 Offer 11. 旋转数组的最小数字.py
@@ -1,9 +1,10 @@
-class Solution:
+class Solution:# 二分法 只能左闭右开结构
     def minArray(self, numbers: List[int]) -> int:
-        i, j = 0, len(numbers) - 1
-        while i < j:
-            m = (i + j) // 2
-            if numbers[m] > numbers[j]: i = m + 1
-            elif numbers[m] < numbers[j]: j = m
-            else: return min(numbers[i:j])
-        return numbers[i]
+        l, r = 0, len(numbers) - 1
+        mid = (l + r) // 2
+        while l < r:    # 最好采用左闭右开结构，种植条件是l==r,可以返回最后那个值
+            mid = (l + r) // 2
+            if numbers[mid] > numbers[r]: l = mid + 1  # 与最右边的值比较
+            elif numbers[mid] < numbers[r]: r = mid
+            else: return min(numbers[l:r])
+        return numbers[l]   # 返回
