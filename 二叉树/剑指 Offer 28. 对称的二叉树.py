3378d7cef041940f401cb87a3e45d39211648519
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-12-17 16:27:34 +0800

    Create 剑指 Offer 28. 对称的二叉树.py

---
+++ 二叉树/剑指 Offer 28. 对称的二叉树.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def recur(L, R):
+            if not L and not R: return True
+            if not L or not R or L.val != R.val: return False
+            return recur(L.left, R.right) and recur(L.right, R.left)
+        if not root: return True
+        return recur(root.left, root.right) # 左右子树是否对称作为函数递归
