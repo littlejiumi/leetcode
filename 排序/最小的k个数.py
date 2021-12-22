28a535210ff1374203881eeed52b23f78ab8e787
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2021-12-22 15:11:56 +0800

    Update 最小的k个数.py

--- 排序/最小的k个数.py
+++ 排序/最小的k个数.py
@@ -3,6 +3,7 @@ class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
         arr.sort()
         return arr[:k]
+# 堆 O(nlogk)
 # 需要一个大根堆维护最小堆k个值 python默认是小根堆因此我们要对数组中所有的数取其相反数
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
@@ -17,3 +18,35 @@ class Solution:
                 heapq.heappush(hp, -arr[i])
         ans = [-x for x in hp]
         return ans
+# 快排思想快排的划分函数每次执行完后都能将数组分成两个部分，小于等于分界值 pivot 的元素的都会被放到数组的左边，大于的都会被放到数组的右边，然后返回分界值的下标。
+# 与快速排序不同的是，快速排序会根据分界值的下标递归处理划分的两侧，而这里我们只处理划分的一边。
+
+class Solution:
+    def partition(self, nums, l, r):
+        pivot = nums[r]
+        i = l - 1
+        for j in range(l, r):
+            if nums[j] <= pivot:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1], nums[r] = nums[r], nums[i + 1]
+        return i + 1
+
+    def randomized_partition(self, nums, l, r):
+        i = random.randint(l, r)
+        nums[r], nums[i] = nums[i], nums[r]
+        return self.partition(nums, l, r)
+
+    def randomized_selected(self, arr, l, r, k):
+        pos = self.randomized_partition(arr, l, r)
+        num = pos - l + 1
+        if k < num:
+            self.randomized_selected(arr, l, pos - 1, k)
+        elif k > num:
+            self.randomized_selected(arr, pos + 1, r, k - num)
+
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        if k == 0:
+            return list()
+        self.randomized_selected(arr, 0, len(arr) - 1, k)
+        return arr[:k]
