5d5e7080c87f9dfc9a8f2fa0e6316b23e04017f2
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-02-09 19:49:43 +0800

    Create 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.py

---
+++ 二叉树/剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val < p.val and root.val < q.val: # p,q 都在 root 的右子树中
+                root = root.right # 遍历至右子节点
+            elif root.val > p.val and root.val > q.val: # p,q 都在 root 的左子树中
+                root = root.left # 遍历至左子节点
+            else: break
+        return root
