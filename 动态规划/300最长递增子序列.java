81b14a4876cfb8594e6745b0acd59e42964d49f9
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-02-21 11:21:22 +0800

    Update 300最长递增子序列.java

--- 动态规划/300最长递增子序列.java
+++ 动态规划/300最长递增子序列.java
@@ -15,3 +15,17 @@ class Solution {
         return res;
     }
 }
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        dp = [1] * len(nums)
+        result = 0
+        for i in range(1, len(nums)):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                # 位置i的最长升序子序列等于j从0到i-1各个位置的最长升序子序列 + 1 的最大值。
+                    dp[i] = max(dp[i], dp[j] + 1)
+            result = max(result, dp[i]) 
+        return result
