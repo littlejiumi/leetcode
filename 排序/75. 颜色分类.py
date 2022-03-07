74853e53660be2ee10e1889219b0fffa1b10c6ba
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-07 15:49:27 +0800

    Create 75. 颜色分类.py

---
+++ 排序/75. 颜色分类.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        L, R=-1, len(nums)
+        i = 0
+        while i < R:
+            if nums[i]==0: # 如果等于 0， 则把 L+1 和 i 替换，并且 i+=1。这里i ++ 是因为i是从左往右遍历的，这里的拿来替换0的元素一定是1而不会出现2。
+                L += 1
+                nums[i], nums[L] = nums[L], nums[i]
+                i+=1
+                
+            elif nums[i]==2: # 如果等于 2， 则把 R-1 和 i 替换，并且 i 会保持原样进行下一轮判定
+                R-=1
+                nums[i], nums[R] = nums[R], nums[i]
+            else:
+                i+=1
+        
