864a525ace39c08b1549099ce05cdb5ec36e836f
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-07 15:13:11 +0800

    Update 堆排序.java

--- 排序/堆排序.java
+++ 排序/堆排序.java
@@ -48,3 +48,36 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        def maxHepify(arr, i, end):     # 大顶堆
+            j = 2*i                     # j为i的左子节点【建堆时下标1表示堆顶】
+            while j <= end:
+                if j+1 <= end and arr[j+1] > arr[j]:    # i的左右子节点分别为j和j+1
+                    j += 1                              # 取两者之间的较大者
+                
+                if arr[i] < arr[j]:             # 若i指示的元素小于其子节点中的较大者
+                    arr[i], arr[j] = arr[j], arr[i]     # 交换i和j的元素，并继续往下判断
+                    i = j                       # 往下走：i调整为其子节点j
+                    j = 2*i                     # j调整为i的左子节点
+                else:                           # 否则，结束调整
+                    break
+        
+        n = len(nums)
+        nums = [0] + nums   # nums头部添加0，满足从下标1开始建堆
+        
+        # 建堆【大顶堆】
+        for i in range(n//2, 0, -1):    # 从第一个非叶子节点n//2开始依次往上进行建堆的调整【注意：此时堆顶为下标1】
+            maxHepify(nums, i, n)
+        
+        # 排序：依次将堆顶元素（当前最大值）放置到尾部，并调整堆
+        for j in range(n, 0, -1):
+            nums[1], nums[j] = nums[j], nums[1]     # nums[1]为堆顶元素（最大值），将其放置到尾部j
+            maxHepify(nums, 1, j-1)                 # j-1变成尾部，并从堆顶1开始调整堆
+        
+        return nums[1:]
