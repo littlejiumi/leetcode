1ed93b38b77d1d58f231c55626a05ef3fd5ec5a6
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-07 14:54:12 +0800

    Update 快速排序.java.py

--- 排序/快速排序.java.py
+++ 排序/快速排序.java.py
@@ -41,25 +41,23 @@ class Solution {
 
 # Python
 class Solution:
-    def randomized_partition(self, nums, l, r):
-        pivot = random.randint(l, r)
-        nums[pivot], nums[r] = nums[r], nums[pivot]
-        i = l - 1
-        for j in range(l, r):
-            if nums[j] < nums[r]:
-                i += 1
-                nums[j], nums[i] = nums[i], nums[j]
-        i += 1
-        nums[i], nums[r] = nums[r], nums[i]
-        return i
-
-    def randomized_quicksort(self, nums, l, r):
-        if r - l <= 0:
-            return
-        mid = self.randomized_partition(nums, l, r)
-        self.randomized_quicksort(nums, l, mid - 1)
-        self.randomized_quicksort(nums, mid + 1, r)
-
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.randomized_quicksort(nums, 0, len(nums) - 1)
+        def randomPart(nums, L, R):
+            pivot = random.randint(L, R)
+            nums[pivot], nums[R] = nums[R], nums[pivot]
+            i = L-1
+            for j in range(L, R):
+                if nums[j]<nums[R]:
+                    i += 1
+                    nums[i],nums[j]= nums[j], nums[i]
+            i+=1
+            nums[R], nums[i] = nums[i],nums[R]
+            return i
+
+        def quickSort(nums, L, R):
+            if L >= R: return
+            mid = randomPart(nums, L, R)
+            quickSort(nums, L, mid-1)
+            quickSort(nums, mid+1, R)
+        quickSort(nums, 0, len(nums)-1)
         return nums
