2561de425695d0c8a0ca26a900212eadd8de4274
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-13 16:12:59 +0800

    Update 剑指 Offer 52. 两个链表的第一个公共节点.py

--- 链表/剑指 Offer 52. 两个链表的第一个公共节点.py
+++ 链表/剑指 Offer 52. 两个链表的第一个公共节点.py
@@ -15,3 +15,20 @@ class Solution:
                 return headB
             headB = headB.next
         return None
+    
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        """
+        两个链表遍历完自己的再遍历另一个到相交节点时，走过的节点数目相同
+
+        那么，只要其中一个链表走完了，就去走另一条链表的路。如果有交点，他们最终一定会在同一个
+        位置相遇
+        """
+        cur_a, cur_b = headA, headB     # 用两个指针代替a和b
+
+        
+        while cur_a != cur_b:
+            cur_a = cur_a.next if cur_a else headB      # 如果a走完了，那么就切换到b走
+            cur_b = cur_b.next if cur_b else headA      # 同理，b走完了就切换到a
+        
+        return cur_a
