4498c107edce27466bf71e933a43dc53aa755aee
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-13 16:54:34 +0800

    Update 142环形链表2.java

--- 链表/142环形链表2.java
+++ 链表/142环形链表2.java
@@ -31,6 +31,24 @@ public class Solution {
     }
 }
 
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            # 如果相遇
+            if slow == fast:
+                p = head
+                q = slow
+                while p!=q:
+                    p = p.next
+                    q = q.next
+                #你也可以return q
+                return p
+
+        return None
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
