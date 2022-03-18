6947e7469158af477d3974e9dda81a61fca61148
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-18 15:03:05 +0800

    Create 剑指 Offer 58 - II. 左旋转字符串.py

---
+++ 字符串/剑指 Offer 58 - II. 左旋转字符串.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        s = list(s)
+        s[0:n] = list(reversed(s[0:n]))
+        s[n:] = list(reversed(s[n:]))
+        s.reverse()
+        
+        return "".join(s)
