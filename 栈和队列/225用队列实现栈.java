f087a7d00ed3811272a67de5969b2dcf30c41caf
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-21 14:34:33 +0800

    Update 225用队列实现栈.java

--- 栈和队列/225用队列实现栈.java
+++ 栈和队列/225用队列实现栈.java
@@ -36,3 +36,37 @@ class MyStack {
         return queue1.isEmpty();
     }
 }
+
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        if self.empty(): return 
+        for i in range(len(self.q1)-1):  # 剩一个就是要popleft的
+            self.q2.append(self.q1.popleft())
+
+        self.q1, self.q2 = self.q2, self.q1 # 交换
+        return self.q2.popleft()
+
+    def top(self) -> int:
+        if self.empty(): return 
+        return self.q1[-1]
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
+
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
