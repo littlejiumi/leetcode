bdbf96f4af95941b024bee43f46d0c57c9c6205e
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-21 19:04:43 +0800

    Update 102二叉树的层序遍历.java

--- 二叉树/102二叉树的层序遍历.java
+++ 二叉树/102二叉树的层序遍历.java
@@ -33,3 +33,27 @@ class Solution {
         return res;
     }
 }
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res, queue = [], []
+        if not root:
+            return res
+        queue.append(root)
+        while len(queue) > 0:
+            temp = []
+            size = len(queue)
+            for _ in range(size):
+                node = queue[0]
+                queue = queue[1:]
+                temp.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(temp)
+        return res
