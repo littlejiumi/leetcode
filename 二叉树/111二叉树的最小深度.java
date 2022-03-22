5897c79c31b38504a9549cf00c9b92d9ed3359e5
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-22 16:07:55 +0800

    Update 111二叉树的最小深度.java

--- 二叉树/111二叉树的最小深度.java
+++ 二叉树/111二叉树的最小深度.java
@@ -46,3 +46,16 @@ class Solution {
         }
     }
 }
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        if root.left and not root.right: return self.minDepth(root.left)+1
+        if root.right and not root.left: return self.minDepth(root.right)+1
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
