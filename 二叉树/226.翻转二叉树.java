dbc6b0f9505ac6e5bb6737fe86207ce42826c617
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-03-22 15:34:15 +0800

    Update 226.翻转二叉树.java

--- 二叉树/226.翻转二叉树.java
+++ 二叉树/226.翻转二叉树.java
@@ -28,3 +28,19 @@ class Solution {
         return root;
     }
 }
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root: return root
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
