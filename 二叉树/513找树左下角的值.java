193f18f311e5c27db3c44aea32c108f7e2eeb902
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-04-06 14:54:21 +0800

    Update 513找树左下角的值.java

--- 二叉树/513找树左下角的值.java
+++ 二叉树/513找树左下角的值.java
@@ -34,3 +34,24 @@ class Solution {
         return layers.get(size-1).get(0);
     }
 }
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root: return
+        queue = [root]      
+        while len(queue)!=0:
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop(0)
+                if i == 0: res = node.val
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+        return res
+        
