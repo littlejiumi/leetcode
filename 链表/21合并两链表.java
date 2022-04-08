c2e7a0d554f97248b944fe67f84b3d78dacfd527
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-04-08 20:24:16 +0800

    Update 21合并两链表.java

--- 链表/21合并两链表.java
+++ 链表/21合并两链表.java
@@ -55,3 +55,17 @@ class Solution:
         if list2:
             pse.next = list2
         return res.next
+        
+        
+# list1[0]+merge(list1[1:],list2)
+# list2[0]+merge(list1,list2[1:])
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
