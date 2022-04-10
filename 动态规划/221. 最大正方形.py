ede49211f8a46ae09a062cb1e9c99d1aa5888243
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-04-10 15:38:44 +0800

    Update 221. 最大正方形.py

--- 动态规划/221. 最大正方形.py
+++ 动态规划/221. 最大正方形.py
@@ -1,19 +1,13 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        if len(matrix) == 0 or len(matrix[0]) == 0:
-            return 0
-        
-        maxSide = 0
-        rows, columns = len(matrix), len(matrix[0])
-        dp = [[0] * columns for _ in range(rows)]
-        for i in range(rows):
-            for j in range(columns):
-                if matrix[i][j] == '1':
-                    if i == 0 or j == 0:
-                        dp[i][j] = 1
-                    else:
-                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
-                    maxSide = max(maxSide, dp[i][j])
-        
-        maxSquare = maxSide * maxSide
-        return maxSquare
+        if len(matrix)==0 or len(matrix[0])==0: return 0
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0]*n for _ in range(m)]
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]=='1':
+                    if i==0 or j==0: dp[i][j]=1
+                    else:dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])+1
+                    res = max(res, dp[i][j])
+        return res**2
