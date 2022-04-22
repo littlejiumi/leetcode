efa75dc804a86bcf635c9917c4c7fe66d9c55bbc
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-04-22 18:37:17 +0800

    Update 560. 和为 K 的子数组(前缀和).py

--- 数组/560. 和为 K 的子数组(前缀和).py
+++ 数组/560. 和为 K 的子数组(前缀和).py
@@ -34,3 +34,26 @@
                 else:
                     d[acc] = 1
             return count
+
+public class Solution {
+   public int subarraySum(int[] nums, int k) {
+        int len = nums.length;
+        // 计算前缀和数组
+        int[] preSum = new int[len + 1];
+        preSum[0] = 0;
+        for (int i = 0; i < len; i++) {
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+
+        int count = 0;
+        for (int left = 0; left < len; left++) {
+            for (int right = left; right < len; right++) {
+                // 区间和 [left..right]，注意下标偏移
+                if (preSum[right + 1] - preSum[left] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
