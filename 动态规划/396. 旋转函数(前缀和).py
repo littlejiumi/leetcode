63ce3d2d1f19c246267dc9381c4dcb6d4bf47271
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-04-22 17:35:10 +0800

    Update 396. 旋转函数(前缀和).py

--- 动态规划/396. 旋转函数(前缀和).py
+++ 动态规划/396. 旋转函数(前缀和).py
@@ -6,5 +6,7 @@ class Solution:
         for i in range(1, len(nums)):
             dp[i] = dp[i-1] + Sum - nums[-i]*len(nums)
         return max(dp)
-        
+ /*
+找规律，这种每次顺时针旋转一次的可以通过找动态规划的公式来计算
+*/
 
