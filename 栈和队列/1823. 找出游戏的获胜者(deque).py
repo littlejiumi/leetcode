6eb38fb898b76847fb27980100064a760d1e0615
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-05-04 21:43:47 +0800

    Update 1823. 找出游戏的获胜者(deque).py

--- 栈和队列/1823. 找出游戏的获胜者(deque).py
+++ 栈和队列/1823. 找出游戏的获胜者(deque).py
@@ -6,3 +6,12 @@ class Solution:
                 q.append(q.popleft())
             q.popleft()
         return q[0]
+    
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        queue = list(range(1, n + 1))
+        offset = 0
+        for _ in range(n - 1):
+            offset = (offset + k - 1) % len(queue)
+            queue = queue[:offset] + queue[offset+1:]
+        return queue[0]
