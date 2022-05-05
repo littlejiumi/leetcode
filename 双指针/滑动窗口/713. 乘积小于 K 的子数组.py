281bd24146191f0266956a9688d16d8ecbb3c0ed
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-05-05 09:17:47 +0800

    Update 713. 乘积小于 K 的子数组.py

--- 双指针/滑动窗口/713. 乘积小于 K 的子数组.py
+++ 双指针/滑动窗口/713. 乘积小于 K 的子数组.py
@@ -5,8 +5,8 @@ class Solution:
         res, left = 0, 0
         for right in range(len(nums)):
             product *= nums[right]
-            while product >= k:
+            while left <= right and product >= k:
                 product /= nums[left]
                 left += 1
-            res += right - left + 1 # 子串个数刚好是r-l+1
+            res += right - left + 1 # 包含右边界值的子串个数刚好是r-l+1
         return res
