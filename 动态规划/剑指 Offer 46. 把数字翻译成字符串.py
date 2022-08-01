c2a4c4b173fb631628a89789133ff4e851abccf4
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2022-08-01 21:53:17 +0800

    Update 剑指 Offer 46. 把数字翻译成字符串.py

--- 动态规划/剑指 Offer 46. 把数字翻译成字符串.py
+++ 动态规划/剑指 Offer 46. 把数字翻译成字符串.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution:  # 和跳台阶问题类似，加一个判断
     def translateNum(self, num: int) -> int:
         s= str(num)
         size = len(s)
@@ -8,4 +8,16 @@ class Solution:
             a = b
             b = c
         return b
+    
+class Solution:
+    def translateNum(self, num: int) -> int:
+        if num < 10: return 1
+        nums = str(num)
+        dp = [1] * len(nums)
+
+        dp[1] = 2 if 10 <= int(nums[:2]) < 26 else 1
+
+        for i in range(2, len(nums)):
+            dp[i] = dp[i-1] + dp[i-2] if 10<=int(nums[i-1:i+1])<26 else dp[i-1]
+        return dp[len(nums)-1]
 
